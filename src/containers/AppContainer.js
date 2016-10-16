0a883735a90da0f29484d6921bb6a9314977aa88
Nicholette <nekonekonik@gmail.com>
2016-10-16 20:39:16 +0800

    Add side drawer

--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -3,12 +3,21 @@ import { browserHistory, Router } from 'react-router'
 import { Provider } from 'react-redux'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import AppBar from 'material-ui/AppBar';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
 
 class AppContainer extends Component {
   static propTypes = {
     routes : PropTypes.object.isRequired,
     store  : PropTypes.object.isRequired
   }
+
+  constructor(props) {
+    super(props);
+    this.state = {open: true};
+  }
+
+  handleToggle = () => this.setState({open: !this.state.open});
 
   shouldComponentUpdate () {
     return false
@@ -25,13 +34,18 @@ class AppContainer extends Component {
               title="Bubble"
               iconClassNameRight="muidocs-icon-navigation-expand-more"
             />
+            <Drawer open={this.state.open}>
+              <div className="sidebar-header"> Bubble</div> 
+              <MenuItem>Menu Item</MenuItem>
+              <MenuItem>Menu Item 2</MenuItem>
+            </Drawer>
             <div style={{ height: '100%' }}>
               <Router history={browserHistory} children={routes} />
             </div>
           </div>
         </MuiThemeProvider> 
       </Provider>
-    )
+    );
   }
 }
 
