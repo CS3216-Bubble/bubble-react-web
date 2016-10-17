f48db9a6a40145c5aa70aacef19fa2997762a4fb
Nicholette <nekonekonik@gmail.com>
2016-10-17 23:38:19 +0800

    Edit chat modal

--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -2,6 +2,7 @@ import React, { Component, PropTypes } from 'react'
 import { browserHistory, Router } from 'react-router'
 import { Provider, connect } from 'react-redux'
 import io from 'socket.io-client';
+import _ from 'lodash';
 
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import AppBar from 'material-ui/AppBar';
@@ -14,6 +15,8 @@ import Dialog from 'material-ui/Dialog';
 import FlatButton from 'material-ui/FlatButton';
 import RaisedButton from 'material-ui/RaisedButton';
 
+const socket = io('http://localhost:3000');
+
 class AppContainer extends Component {
   static propTypes = {
     routes : PropTypes.object.isRequired,
@@ -24,6 +27,13 @@ class AppContainer extends Component {
     super(props, context);
     this.state = {
       sideBarOpen: true,
+      chat: {
+        user: '',
+        roomName: '',
+        roomDescription: '',
+        userLimit: '',
+        categories: '',
+      },
       chatName: '',
       addChatModal: false,
     };
@@ -39,21 +49,24 @@ class AppContainer extends Component {
     event.preventDefault();
     this.setState({addChatModal: false});
   }
-  handleModalChange = (event) => {
-    this.setState({chatName: event.target.value});
+  handleModalRoomNameChange = (event) => {
+    this.setState({chat: _.extend(this.state.chat, {roomName: event.target.value}) });
+  }
+  handleModalRoomDescriptionChange = (event) => {
+    this.setState({chat: _.extend(this.state.chat, {roomDescription: event.target.value}) });
+  }
+  handleModalUserLimitChange = (event) => {
+    this.setState({chat: _.extend(this.state.chat, {userLimit: event.target.value}) });
   }
   handleModalSubmit = (event) => {
-    // const { dispatch } = this.props;
     event.preventDefault();
-
     const newChat = {
       name: this.state.chatName.trim(),
       id: `${Date.now()}${uuid.v4()}`,
       private: false
     };
-    // dispatch(actions.createChat(newChat));
     this.handleChangeChat(newChat);
-    socket.emit('new chat', newChat);
+    socket.emit('create_room', newChat);
     this.setState({chatName: ''});
     this.closeAddChatModal();
   }
@@ -64,8 +77,15 @@ class AppContainer extends Component {
     // }).length > 0) {
     //   return 'error';
     // }
-    // return 'success';
     return 'success';
+  }
+  validateUserLimit = () => {
+    var pattern = /^[0-9]+$|^$/; // get whole numbers
+    if (pattern.test(this.state.chat.userLimit)) {
+      return 'success';
+    } else {
+      return 'error';
+    }
   }
 
   render () {
@@ -79,21 +99,33 @@ class AppContainer extends Component {
       <FlatButton
         label="Submit"
         primary={true}
-        disabled={this.validateChatName() === 'error' && 'true'}
+        disabled={this.validateChatName() === 'error' || 'true' && this.validateUserLimit() === 'error'}
         onTouchTap={this.handleModalSubmit}
       />,
     ]
     const newChatModal = (
       <div>
         <Dialog open={this.state.addChatModal} title="Add New Chat" modal={true} actions={newChatActions}>
-          <form onSubmit={::this.handleModalSubmit} >
           <TextField
             errorText={this.validateChatName() === 'error' && 'A chat with that name already exists!'}
-            floatingLabelText="Chat Name"
-            value={this.state.chatName}
-            onChange={::this.handleModalChange}
+            onChange={::this.handleModalRoomNameChange}
+            floatingLabelText="Name"
+            value={this.state.chat.roomName}
           />
-          </form>
+          <br />
+          <TextField
+            floatingLabelText="Description"
+            onChange={::this.handleModalRoomDescriptionChange}
+            value={this.state.chat.roomDescription}
+          />
+          <br />
+          <TextField
+            errorText={this.validateUserLimit() === 'error' && 'User limit must be a number!'}
+            floatingLabelText="User Limit"
+            onChange={::this.handleModalUserLimitChange}
+            value={this.state.chat.userLimit}
+          />
+          <br />
         </Dialog>
       </div>
     );
